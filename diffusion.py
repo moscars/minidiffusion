bac4a122b1f0c7dd6e105143bd5f50110c5defe8
moscars <marcus.oscarsson9@gmail.com>
2023-10-09 17:22:40 +0200

    forward process

--- diffusion.py
+++ diffusion.py
@@ -1,6 +1,21 @@
+import numpy as np
 
 class Diffusion:
     def __init__(self):
-        pass
-    
-    
+        # prepare for forward noising
+        self.beta_start = 0.0001
+        self.beta_end = 0.02
+        self.noising_steps = 1000
+
+        self.betas = np.linspace(self.beta_start, self.beta_end, self.noising_steps)
+        self.alphas = 1 - self.betas
+        self.alpha_hats = np.cumprod(self.alphas)
+
+    def forward(self, image, target_t):
+        ''' 
+        image: input image to be noised (3072,)
+        '''
+        cumulative_mean = image * np.sqrt(self.alpha_hats[target_t])
+        cumulative_variance = 1 - self.alphas[target_t]
+        image_t = np.random.normal(cumulative_mean, np.sqrt(cumulative_variance))
+        return image_t
--- main.py
+++ main.py
@@ -1,31 +1,22 @@
-import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
-def show_image(data_row):
-    # Reshape the data to 3x32x32 and transpose it to 32x32x3
-    image = data_row.reshape(3, 32, 32).transpose(1, 2, 0)
-    plt.imshow(image)
-    plt.show()
-
-def show_images_grid(data):
-    fig, axes = plt.subplots(2, 2)  # 2x2 grid
-    for i, ax in enumerate(axes.flat):
-        image = data[i].reshape(3, 32, 32).transpose(1, 2, 0)
-        ax.imshow(image)
-        ax.axis('off')  # Hide axes for better visualization
-    plt.show()
+from diffusion import Diffusion
+from utils import *
 
 def extract_horses(filename, horses):
     data = unpickle(filename)
     for i in range(len(data[b'labels'])):
         if data[b'labels'][i] == 7:
             horses.append(data[b'data'][i])
+
+def squeeze01(image):
+    return (image - 255) / 255
+
+def normalize(image):
+    return (image * 2) - 1
+
+def unnormalize(image):
+    return (image + 1) / 2
 
 if __name__ == '__main__':
     horses = []
@@ -35,6 +26,9 @@ if __name__ == '__main__':
     extract_horses('data/data_batch_4', horses)
     extract_horses('data/data_batch_5', horses)
     extract_horses('data/test_batch', horses)
-    
-    show_images_grid(horses)
-    print(len(horses))
+
+    horses = np.array([squeeze01(x) for x in horses])
+    show_image(horses[0])
+    d = Diffusion()
+    noised = d.forward(normalize(horses[0]), 999)
+    show_image(unnormalize(noised))
