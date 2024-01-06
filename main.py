8f940c76cbbe25a55bebf917f48976c0582ecd58
moscars <marcus.oscarsson9@gmail.com>
2024-01-06 17:58:27 +0100

    training 32

--- main.py
+++ main.py
@@ -70,21 +70,21 @@ def train(diffusion, lr, num_epochs, dataset, batch_size):
                 print(f"Step {i} of {num_batches} Long: {round(longLoss, 6)}, Current: {round(loss.item(), 6)}")
         
         if epoch > 3 and epoch % 5 == 0:
-            torch.save(diffusion.model.state_dict(), f"base_model64_{epoch}.pt")
-            torch.save(optimizer.state_dict(), f"base_optimizer64_{epoch}.pt")
+            torch.save(diffusion.model.state_dict(), f"6_jan_model32_{epoch}.pt")
+            torch.save(optimizer.state_dict(), f"6_jan_optimizer32_{epoch}.pt")
 
         if epoch > 0 and epoch % 10 == 0:
-            show_4_images(diffusion.generate(4, 0)[0], save=True, name=f"11_base_64_0_{epoch}")
-            show_4_images(diffusion.generate(4, 1)[0], save=True, name=f"11_base_64_1_{epoch}")
-            show_4_images(diffusion.generate(4, 2)[0], save=True, name=f"11_base_64_2_{epoch}")
-            show_4_images(diffusion.generate(4, 3)[0], save=True, name=f"11_base_64_3_{epoch}")
+            show_4_images(diffusion.generate(4, 0)[0], save=True, name=f"6_jan_base_32_0_{epoch}")
+            show_4_images(diffusion.generate(4, 1)[0], save=True, name=f"6_jan_base_32_1_{epoch}")
+            show_4_images(diffusion.generate(4, 2)[0], save=True, name=f"6_jan_base_32_2_{epoch}")
+            show_4_images(diffusion.generate(4, 3)[0], save=True, name=f"6_jan_base_32_3_{epoch}")
             ema_model = ema.getEMAModel()
             tmpDiff = Diffusion(device=device, num_classes=4, img_size=64, in_channels=3)
             tmpDiff.model = ema_model
-            show_4_images(tmpDiff.generate(4, 0)[0], save=True, name=f"11_base_64_0_{epoch}_ema")
-            show_4_images(tmpDiff.generate(4, 1)[0], save=True, name=f"11_base_64_1_{epoch}_ema")
-            show_4_images(tmpDiff.generate(4, 2)[0], save=True, name=f"11_base_64_2_{epoch}_ema")
-            show_4_images(tmpDiff.generate(4, 3)[0], save=True, name=f"11_base_64_3_{epoch}_ema")
+            show_4_images(tmpDiff.generate(4, 0)[0], save=True, name=f"6_jan_base_32_0_{epoch}_ema")
+            show_4_images(tmpDiff.generate(4, 1)[0], save=True, name=f"6_jan_base_32_1_{epoch}_ema")
+            show_4_images(tmpDiff.generate(4, 2)[0], save=True, name=f"6_jan_base_32_2_{epoch}_ema")
+            show_4_images(tmpDiff.generate(4, 3)[0], save=True, name=f"6_jan_base_32_3_{epoch}_ema")
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else 'cpu')
@@ -96,12 +96,12 @@ if __name__ == '__main__':
 
     images = []
     labels = []
-    # extract_all_data('data/data_batch_1', images, labels, classes)
-    # extract_all_data('data/data_batch_2', images, labels, classes)
-    # extract_all_data('data/data_batch_3', images, labels, classes)
-    # extract_all_data('data/data_batch_4', images, labels, classes)
-    # extract_all_data('data/data_batch_5', images, labels, classes)
-    # extract_all_data('data/test_batch', images, labels, classes)
+    extract_all_data('data/data_batch_1', images, labels, classes)
+    extract_all_data('data/data_batch_2', images, labels, classes)
+    extract_all_data('data/data_batch_3', images, labels, classes)
+    extract_all_data('data/data_batch_4', images, labels, classes)
+    extract_all_data('data/data_batch_5', images, labels, classes)
+    extract_all_data('data/test_batch', images, labels, classes)
     # read_binary_data('stl10_binary/train_X.bin', 'stl10_binary/train_Y.bin', images, labels, classes)
     # images, labels = read_lineaus('linnaeus32/train')
     # images, labels = read_cifar_10_64('cifar10-64/train')
@@ -109,20 +109,21 @@ if __name__ == '__main__':
     # images.extend(images_test)
     # labels.extend(labels_test)
 
-    # images = np.array(images)
-    # labels = np.array(labels)
+    images = np.array(images)
+    labels = np.array(labels)
+    images = np.reshape(images, (images.shape[0], 3, 32, 32))
+    
 
-#    images = np.reshape(images, (images.shape[0], 3, 32, 32))
     # # # #save images and labels to file
     # np.save('images.npy', images)
     # np.save('labels.npy', labels)
     # exit()
     #read images and labels from file
-    images = np.load('images.npy')
-    labels = np.load('labels.npy')
+    # images = np.load('images.npy')
+    # labels = np.load('labels.npy')
     images = np.array([normalize(squeeze01(x)) for x in images])
     labels = np.array(labels)
-    
+
     images = torch.tensor(images, dtype=torch.float32)
     labels = torch.tensor(labels, dtype=torch.int32)
 
@@ -145,10 +146,10 @@ if __name__ == '__main__':
 
     print(f"Length of dataset: {len(dataset)}")
 
-    diffusion = Diffusion(device=device, num_classes=4, img_size=64, in_channels=3)
+    diffusion = Diffusion(device=device, num_classes=4, img_size=32, in_channels=3)
 
     # first 30 epochs with lr 1e-4
-    train(diffusion, 1e-4, 500, dataset, batch_size=4)
+    train(diffusion, 6e-4, 500, dataset, batch_size=24)
 
     end = time.time()
     print(f"Total time: {end - start}")
